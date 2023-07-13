131b2498d77aa7dca2e52c9f7f2d7a31868a8d4c
poodlepoodle <chammal97@naver.com>
2023-07-14 01:47:21 +0900

    feat(CameraView): add flashlight toggle

--- AllerView/CameraView.swift
+++ AllerView/CameraView.swift
@@ -10,6 +10,10 @@ import CarBode
 
 struct CameraView: View {
     
+    // MARK: - Variables
+        
+    @State private var isFlashOn = false
+    
     var body: some View {
         
         ZStack {
@@ -17,6 +21,7 @@ struct CameraView: View {
             
             CBScanner(
                 supportBarcode: .constant([.codabar, .code39, .code39Mod43, .code93, .code128, .ean8, .ean13, .gs1DataBar, .gs1DataBarLimited, .gs1DataBarExpanded, .interleaved2of5, .itf14, .upce]),
+                torchLightIsOn: $isFlashOn,
                 scanInterval: .constant(5)
             ) { barcode in
                 print("BarCodeType =", barcode.type.rawValue, "Value =", barcode.value)
@@ -35,6 +40,29 @@ struct CameraView: View {
                 let fillColor = UIColor(red: 0, green: 1, blue: 0.2, alpha: 0.4)
                 
                 barcodeView.draw(lineWidth: lineWidth, lineColor: lineColor, fillColor: fillColor)
+            }
+            
+            // MARK: - Detecting Box
+            
+            // add something to here
+            
+            // MARK: - Flash Toggle Button
+            
+            VStack {
+                Spacer()
+                
+                HStack {
+                    Spacer()
+                    Button(action: {
+                        isFlashOn.toggle()
+                    }) {
+                        Image(isFlashOn ? "icon_flash_on" : "icon_flash_off")
+                            .resizable()
+                            .frame(width: 50, height: 50)
+                    }
+                    .padding(.trailing, 46)
+                    .padding(.bottom, 85)
+                }
             }
         }.edgesIgnoringSafeArea(.all)
     }
