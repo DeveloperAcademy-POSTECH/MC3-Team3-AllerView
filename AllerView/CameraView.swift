0224f3b42f29c86aaf2627c2efdecceb5de90c84
poodlepoodle <chammal97@naver.com>
2023-07-14 01:20:25 +0900

    build(): add permission to use camera

---
+++ AllerView/CameraView.swift
@@ -0,0 +1,20 @@
+//
+//  CameraView.swift
+//  AllerView
+//
+//  Created by Eojin Choi on 2023/07/13.
+//
+
+import SwiftUI
+
+struct CameraView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CameraView_Previews: PreviewProvider {
+    static var previews: some View {
+        CameraView()
+    }
+}
