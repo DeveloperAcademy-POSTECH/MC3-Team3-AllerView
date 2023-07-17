68b5e8bad3f765ff44ce925502e3e59dc43c3892
ckyeon <qsky8520@gmail.com>
2023-07-18 00:41:06 +0900

    feat(ProductDetailView): add proudct detail view

---
+++ AllerView/Extension/Font+Custom.swift
@@ -0,0 +1,16 @@
+//
+//  Font+Custom.swift
+//  AllerView
+//
+//  Created by 조기연 on 2023/07/13.
+//
+
+import SwiftUI
+
+extension Font {
+    public static let customTitle: Font = .system(size: 30, weight: .bold)
+    
+    public static let customHeadline: Font = .system(size: 20, weight: .semibold)
+    
+    public static let customBody: Font = .system(size: 17)
+}
