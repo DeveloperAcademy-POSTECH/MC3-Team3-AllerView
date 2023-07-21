14c6a29a53ccca000839505b44c599786fe78bc9
snack <kwansiknam@gmail.com>
2023-07-22 06:53:14 +0900

    feat(ScanImage): add custom cameraview

---
+++ AllerView/View/CustomScannerView.swift
@@ -0,0 +1,20 @@
+//
+//  CustomScannerView.swift
+//  AllerView
+//
+//  Created by 관식 on 2023/07/21.
+//
+
+import SwiftUI
+
+struct CustomScannerView: View {
+    var body: some View {
+        CustomCameraView()
+    }
+}
+
+struct CustomScannerView_Previews: PreviewProvider {
+    static var previews: some View {
+        CustomCameraView()
+    }
+}
