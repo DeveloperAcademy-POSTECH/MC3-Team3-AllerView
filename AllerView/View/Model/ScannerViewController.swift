855a4b89ae57aec6caeba7ad33a6c07dc58904f6
poodlepoodle <chammal97@naver.com>
2023-08-05 20:55:31 +0900

    feat(ScannerView): add ScannerViewController

---
+++ AllerView/View/Model/ScannerViewController.swift
@@ -0,0 +1,136 @@
+//
+//  ScannerViewController.swift
+//  AllerView
+//
+//  Created by Eojin Choi on 2023/08/05.
+//
+
+import SwiftUI
+import Vision
+import VisionKit
+
+@MainActor
+struct ScannerViewController: UIViewControllerRepresentable {
+    static let textDataType: DataScannerViewController.RecognizedDataType = .text(
+        languages: [
+            "ko-KR",
+            "en-US"
+        ]
+    )
+    
+    var scannerViewController: DataScannerViewController = DataScannerViewController(
+        recognizedDataTypes: [ScannerViewController.textDataType],
+        qualityLevel: .accurate,
+        recognizesMultipleItems: false,
+        isHighFrameRateTrackingEnabled: true,
+        isPinchToZoomEnabled: true,
+        isGuidanceEnabled: false,
+        isHighlightingEnabled: true
+    )
+    
+    var scannerAvailable: Bool {
+        DataScannerViewController.isSupported &&
+        DataScannerViewController.isAvailable
+    }
+    
+    func makeUIViewController(context: Context) -> DataScannerViewController {
+        scannerViewController.delegate = context.coordinator
+        
+        return scannerViewController
+    }
+    
+    func updateUIViewController(_ uiViewController: DataScannerViewController, context: Context) {}
+    
+    func makeCoordinator() -> Coordinator {
+        return Coordinator(self)
+    }
+    
+    class Coordinator: NSObject, DataScannerViewControllerDelegate {
+        var parent: ScannerViewController
+        var roundBoxMappings: [UUID: UIView] = [:]
+        
+        init(_ parent: ScannerViewController) {
+            self.parent = parent
+        }
+        
+        func dataScanner(_ dataScanner: DataScannerViewController, didAdd addedItems: [RecognizedItem], allItems: [RecognizedItem]) {
+            processAddedItems(items: addedItems)
+        }
+        
+        func dataScanner(_ dataScanner: DataScannerViewController, didRemove removedItems: [RecognizedItem], allItems: [RecognizedItem]) {
+            processRemovedItems(items: removedItems)
+        }
+        
+        func dataScanner(_ dataScanner: DataScannerViewController, didUpdate updatedItems: [RecognizedItem], allItems: [RecognizedItem]) {
+            processUpdatedItems(items: updatedItems)
+        }
+
+        func processAddedItems(items: [RecognizedItem]) {
+            for item in items {
+                processItem(item: item)
+            }
+        }
+        
+        func processRemovedItems(items: [RecognizedItem]) {
+            for item in items {
+                removeRoundBoxFromItem(item: item)
+            }
+        }
+        
+        func processUpdatedItems(items: [RecognizedItem]) {
+            for item in items {
+                updateRoundBoxToItem(item: item)
+            }
+        }
+        
+        func addRoundBoxToItem(frame: CGRect, text: String, item: RecognizedItem) {
+            let roundedRectView = RoundedRectLabel(frame: frame)
+            roundedRectView.setText(text: "원재료명 부분")
+            parent.scannerViewController.overlayContainerView.addSubview(roundedRectView)
+            roundBoxMappings[item.id] = roundedRectView
+        }
+        
+        func removeRoundBoxFromItem(item: RecognizedItem) {
+            if let roundBoxView = roundBoxMappings[item.id] {
+                if roundBoxView.superview != nil {
+                    roundBoxView.removeFromSuperview()
+                    roundBoxMappings.removeValue(forKey: item.id)
+                }
+            }
+        }
+        
+        func updateRoundBoxToItem(item: RecognizedItem) {
+            if let roundBoxView = roundBoxMappings[item.id] {
+                if roundBoxView.superview != nil {
+                    let frame = getRoundBoxFrame(item: item)
+                    roundBoxView.frame = frame
+                }
+            }
+        }
+        
+        func getRoundBoxFrame(item: RecognizedItem) -> CGRect {
+            let frame = CGRect(
+                x: item.bounds.topLeft.x,
+                y: item.bounds.topLeft.y,
+                width: abs(item.bounds.topRight.x - item.bounds.topLeft.x) + 15,
+                height: abs(item.bounds.topLeft.y - item.bounds.bottomLeft.y) + 15
+            )
+            return frame
+        }
+        
+        func processItem(item: RecognizedItem) {
+            switch item {
+            case .text(let text):
+                print("Text transcript - \(text.transcript)")
+                if text.transcript.contains("재료명") {
+                    let frame = getRoundBoxFrame(item: item)
+                    addRoundBoxToItem(frame: frame, text: text.transcript, item: item)
+                }
+            case .barcode:
+                break
+            @unknown default:
+                print("Should not happen")
+            }
+        }
+    }
+}
