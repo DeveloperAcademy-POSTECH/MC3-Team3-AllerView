d3add151670b40650ba7cab845c07c5a4471fd43
ckyeon <qsky8520@gmail.com>
2023-08-05 13:44:04 +0900

    refactor(Project): refactoring

--- AllerView/View/Model/GPTModel.swift
+++ AllerView/View/Model/GPTModel.swift
@@ -15,58 +15,51 @@ class GPTModel: ObservableObject {
     @Published var gptResponse: OpenAIChat.Content?
     @Published var isFailed: Bool = false
 
-    private var allergyScript: String {
+    private var actScript: String {
+        "Act like an allergist."
+    }
+
+    private var jsonScript: String {
         """
         <ingredients>\(scannedData)<ingredients>
-
         <allergies>\(allergies)<allergies>
 
-        1. Act like an allergist.
-        2. json을 만들어줘.
+        json을 만들어줘.
         키는 allergenic_ingredients", "all_ingredients", "unrecognized_ingredients"가 있어.
-        "allergenic_ingredients"에는 <allergies>를 유발하는 <ingredients>를 넣어.
+        "allergenic_ingredients"에는 <ingredients>에서 <allergies>를 유발하는 것을 넣어
         "all_ingredients"에는 <ingredients>를 넣어.
-        "unrecognized_ingredients"에는 원재료가 아닌 것을 넣어.
+        "unrecognized_ingredients"에는 <ingredients>에서 원재료가 아닌 것을 넣어.
         """
     }
 
     private var translateScript: String {
-        "영어로 번역해줘."
+        """
+        영어로 번역해줘.
+        """
     }
 
     private let openAIService = OpenAIService()
 
+    @MainActor
     func sendMessage() {
-        let allergyMessage = OpenAIChat.Message(role: .user, content: allergyScript)
+        let actMessage = OpenAIChat.Message(role: .user, content: actScript)
+        let jsonMessage = OpenAIChat.Message(role: .user, content: jsonScript)
         let translateMessage = OpenAIChat.Message(role: .user, content: translateScript)
 
         Task {
-            let tempResponse = await openAIService.sendMessage(messages: [allergyMessage])
-            guard let tempMessage = tempResponse?.choices.first?.message else {
-                DispatchQueue.main.async {
-                    self.isFailed = true
-                }
-                return
-            }
+            do {
+                let actResponseMessage = try await self.fetchMessage(messages: [actMessage])
+                let jsonResponseMessage = try await self.fetchMessage(messages: [actMessage, actResponseMessage, jsonMessage])
+                let translateResponseMessage = try await self.fetchMessage(messages: [actMessage, actResponseMessage, jsonMessage, jsonResponseMessage, translateMessage])
 
-            let response = await openAIService.sendMessage(messages: [allergyMessage, tempMessage, translateMessage])
-            guard let result = response?.choices.first?.message.content.getJson().data(using: .utf8) else {
-                DispatchQueue.main.async {
-                    self.isFailed = true
-                }
-                return
-            }
-            
-            print("=== temp ===")
-            print(tempMessage.content)
+                print(actResponseMessage.content)
+                print(jsonResponseMessage.content)
+                print(translateResponseMessage.content)
 
-            await MainActor.run {
-                do {
-                    self.gptResponse = try JSONDecoder().decode(OpenAIChat.Content.self, from: result)
-                } catch {
-                    print(error.localizedDescription)
-                    isFailed = true
-                }
+                guard let contentData = translateResponseMessage.content.data(using: .utf8) else { fatalError() }
+                self.gptResponse = try JSONDecoder().decode(OpenAIChat.Content.self, from: contentData)
+            } catch {
+                self.isFailed = true
             }
         }
     }
@@ -82,5 +75,11 @@ class GPTModel: ObservableObject {
         allergies = ""
         scannedData = ""
         isFailed = false
+    }
+
+    private func fetchMessage(messages: [OpenAIChat.Message]) async throws -> OpenAIChat.Message {
+        let response = await openAIService.sendMessage(messages: messages)
+        guard let responseMessage = response?.choices.first?.message else { fatalError() }
+        return responseMessage
     }
 }
